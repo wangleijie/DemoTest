4f703aa0a623a51d6f0485d10b4cef17cac475ac
Mr.wang <651893915@qq.com>
2017-09-28 15:13:29 +0800

    第一次提交, 添加工程文件

---
+++ src/main/java/com/lanou/test/MainTest.java
@@ -0,0 +1,10 @@
+package com.lanou.test;
+
+/**
+ * Created by dllo on 17/9/28.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("????");
+    }
+}
