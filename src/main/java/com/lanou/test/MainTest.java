021a81a8f191879780968ac954066ccad4aaf238
Mr.wang <651893915@qq.com>
2017-09-28 15:18:20 +0800

    在MainTest文件中添加一行输出语句

--- src/main/java/com/lanou/test/MainTest.java
+++ src/main/java/com/lanou/test/MainTest.java
@@ -6,5 +6,9 @@ package com.lanou.test;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("????");
+        System.out.println("吃饭吧!");
+
+
+
     }
 }
